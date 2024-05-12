59667261081f7e8bbe4cb442f77cdaf77966f1c1
Brij Duggal <Brij.Duggal@mpft.nhs.uk>
2024-05-12 08:56:13 +0100

    First pass complete

---
+++ Data/MotTests.cs
@@ -0,0 +1,9 @@
+﻿namespace BupaDental.BrijDuggalTest.Data
+{
+    public class MotTests
+    {
+        public string? OdometerValue { get; set; }
+        public string? ExpiryDate { get; set; }
+
+    }
+}
---
+++ Data/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿namespace BupaDental.BrijDuggalTest.Data
+{
+    public class Vehicle
+    {
+        public string? Registration { get; set; }
+
+        public string? Make { get; set; }
+
+        public string? Model { get; set; }
+
+        public string? PrimaryColour { get; set; }
+
+        public string? ManufactureYear { get; set; }
+
+        public string? FuelType { get; set; }
+
+        public DateTime? MotTestExpiryDate { get; set; }
+
+        public string? MileageOnLastMot { get; set; }
+
+        public List<MotTests> MotTests { get; set; } = new List<MotTests>();
+
+    }
+}
